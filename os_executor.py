a63946f4cfd0dc83f29540df902d8ec8fe4cb1bd
akinovak <akinovak@gmail.com>
2019-11-14 23:14:49 +0100

    minor fix

--- os_executor.py
+++ os_executor.py
@@ -8,6 +8,6 @@ while True:
     os.system('python3 crawler_process.py ' + 'sold_spider')
     ctx.log.info('loop finished')
     end_time = datetime.now()
-    diff = int((start_time-end_time).total_seconds())
+    diff = int((end_time - start_time).total_seconds())
     sleep_time = 24*60*60 - diff
     time.sleep(sleep_time)
