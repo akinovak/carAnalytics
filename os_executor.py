88489dcfabff417978e722e5aedd72e8cb07249d
akinovak <akinovak@gmail.com>
2019-11-14 23:12:31 +0100

    waitn for one day

--- os_executor.py
+++ os_executor.py
@@ -1,8 +1,13 @@
 import os
 import time
 from conf import ctx
+from datetime import datetime
 while True:
+    start_time = datetime.now()
     os.system('python3 crawler_process.py ' + 'polovni_scrap')
     os.system('python3 crawler_process.py ' + 'sold_spider')
     ctx.log.info('loop finished')
-    time.sleep(ctx.server_timeout)
+    end_time = datetime.now()
+    diff = int((start_time-end_time).total_seconds())
+    sleep_time = 24*60*60 - diff
+    time.sleep(sleep_time)
